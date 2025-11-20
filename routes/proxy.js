8efe102ba2ad0a074b4a4a2e6694ba2df9db8a84
JMesino Gutierrez <127280930+PGD0@users.noreply.github.com>
2025-11-20 15:33:08 -0500

    Revert "agregando headers"
    This reverts commit d8f2099d4d9bdbefaa801d57f6849cd77ecf802d.

--- routes/proxy.js
+++ routes/proxy.js
@@ -7,9 +7,6 @@ const router = Router();
 router.get('/resource', async (req, res, next) => {
   try {
     const data = await getResource('/fetch-all', req.query);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.json(data);
   } catch (err) {
     next(err);
